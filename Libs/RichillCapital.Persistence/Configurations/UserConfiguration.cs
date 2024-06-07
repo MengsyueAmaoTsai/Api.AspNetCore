f647f9db45761dae3ebdecb9a4130b15f66987ab
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-07 18:57:52 +0800

    add user configuration

---
+++ Libs/RichillCapital.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using RichillCapital.Domain;
+
+namespace RichillCapital.Persistence.Configurations;
+
+internal sealed class UserConfiguration :
+    IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .ToTable("users")
+            .HasKey(user => user.Id);
+
+        builder
+            .Property(user => user.Id)
+            .HasColumnName("id")
+            .HasMaxLength(UserId.MaxLength)
+            .HasConversion(
+                id => id.Value,
+                value => UserId.From(value).Value)
+            .IsRequired();
+
+        builder
+            .Property(user => user.Email)
+            .HasColumnName("email")
+            .HasMaxLength(Email.MaxLength)
+            .HasConversion(
+                email => email.Value,
+                value => Email.From(value).Value)
+            .IsRequired();
+    }
+}
