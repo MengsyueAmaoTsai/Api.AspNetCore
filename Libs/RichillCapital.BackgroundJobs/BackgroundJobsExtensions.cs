d48c3b625f2be725a96376ce6202226bdaf7695b
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-16 16:38:10 +0800

    add background jobs

---
+++ Libs/RichillCapital.BackgroundJobs/BackgroundJobsExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RichillCapital.BackgroundJobs;
+
+public static class BackgroundJobsExtensions
+{
+    public static IServiceCollection AddBackgroundJobs(this IServiceCollection services)
+    {
+        return services;
+    }
+}
