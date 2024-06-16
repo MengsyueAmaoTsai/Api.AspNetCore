69d903fcade1099950747d88e296ad81404e7dd6
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-16 16:45:33 +0800

    update ef core configuration

--- Libs/RichillCapital.Persistence/Configurations/AuditLogConfiguration.cs
+++ Libs/RichillCapital.Persistence/Configurations/AuditLogConfiguration.cs
@@ -5,7 +5,8 @@ using RichillCapital.Domain;
 
 namespace RichillCapital.Persistence.Configurations;
 
-internal sealed class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
+internal sealed class AuditLogConfiguration :
+    IEntityTypeConfiguration<AuditLog>
 {
     public void Configure(EntityTypeBuilder<AuditLog> builder)
     {
@@ -20,6 +21,15 @@ internal sealed class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
             .HasConversion(
                 id => id.Value,
                 value => AuditLogId.From(value).Value)
+            .IsRequired();
+
+        builder
+            .Property(log => log.UserId)
+            .HasColumnName("user_id")
+            .HasMaxLength(UserId.MaxLength)
+            .HasConversion(
+                id => id.Value,
+                value => UserId.From(value).Value)
             .IsRequired();
     }
 }
