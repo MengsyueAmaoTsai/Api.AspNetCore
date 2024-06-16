dd137bc68020f6678b1a02205ff02239b5089f14
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-16 16:44:15 +0800

    add properties to audit log

--- Libs/RichillCapital.Domain/AuditLog.cs
+++ Libs/RichillCapital.Domain/AuditLog.cs
@@ -1,11 +1,59 @@
 using RichillCapital.SharedKernel;
+using RichillCapital.SharedKernel.Monads;
 
 namespace RichillCapital.Domain;
 
 public sealed class AuditLog : Entity<AuditLogId>
 {
-    private AuditLog(AuditLogId id)
+    private AuditLog(
+        AuditLogId id,
+        UserId userId,
+        string userName,
+        string action,
+        string objectId,
+        string log,
+        DateTimeOffset createdTime)
         : base(id)
     {
+        UserId = userId;
+        UserName = userName;
+        Action = action;
+        ObjectId = objectId;
+        Log = log;
+        CreatedTime = createdTime;
+    }
+
+    public UserId UserId { get; private set; }
+
+    public string UserName { get; private set; }
+
+    public string Action { get; private set; }
+
+    public string ObjectId { get; private set; }
+
+    public string Log { get; private set; }
+
+    public DateTimeOffset CreatedTime { get; private set; }
+
+    public static ErrorOr<AuditLog> Create(
+        AuditLogId id,
+        UserId userId,
+        string userName,
+        string action,
+        string objectId,
+        string log,
+        DateTimeOffset createdTime)
+    {
+        var auditLog = new AuditLog(
+            id,
+            userId,
+            userName,
+            action,
+            objectId,
+            log,
+            createdTime);
+
+        return auditLog
+            .ToErrorOr();
     }
 }
