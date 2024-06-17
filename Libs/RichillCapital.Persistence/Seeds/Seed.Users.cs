6e0bd13b2d08d34dd60e398fd969450994f12793
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 15:15:12 +0800

    add users

--- Libs/RichillCapital.Persistence/Seeds/Seed.Users.cs
+++ Libs/RichillCapital.Persistence/Seeds/Seed.Users.cs
@@ -10,6 +10,8 @@ public static partial class Seed
     {
         yield return CreateUser("UID0000001", "Mengsyue Tsai", "mengsyue.tsai@outlook.com", "+886903776473");
         yield return CreateUser("UID0000002", "Mengsyue Tsai", "mengsyue.tsai@gmail.com", "+886903776473");
+        yield return CreateUser("UID0000003", "Copy Trader User", "copytrader@gmail.com", "+886903776473");
+        yield return CreateUser("UID0000004", "Community User", "anonymous@gmail.com", "+886903776473");
     }
 
     private static User CreateUser(
