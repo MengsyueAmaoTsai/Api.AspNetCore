a3e92dd6a5905626aa96bc669c14805ad9a5b05b
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-21 18:23:51 +0800

    Update Domain to 1.0.0.5

---
+++ Libs/RichillCapital.Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using RichillCapital.Domain;
+using RichillCapital.Domain.Users;
+
+namespace RichillCapital.Persistence.Configurations;
+
+internal sealed class AccountConfiguration :
+    IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder
+            .ToTable("accounts")
+            .HasKey(account => account.Id);
+
+        builder
+            .Property(account => account.Id)
+            .HasColumnName("id")
+            .HasMaxLength(AccountId.MaxLength)
+            .HasConversion(
+                id => id.Value,
+                value => AccountId.From(value).Value)
+            .IsRequired();
+
+        builder
+            .Property(account => account.UserId)
+            .HasColumnName("user_id")
+            .HasMaxLength(UserId.MaxLength)
+            .HasConversion(
+                id => id.Value,
+                value => UserId.From(value).Value)
+            .IsRequired();
+
+        builder
+            .HasOne<User>()
+            .WithMany(user => user.Accounts)
+            .HasForeignKey("user_id");
+    }
+}
