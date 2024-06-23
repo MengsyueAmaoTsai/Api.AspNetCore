ff5e3f5222f6817fa801743d9ca1403e9622ac7e
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-24 02:47:59 +0800

    Create signal source tests

--- Tests/RichillCapital.Api.AcceptanceTests/SignalSources/CreateSignalSourceTests.cs
+++ Tests/RichillCapital.Api.AcceptanceTests/SignalSources/CreateSignalSourceTests.cs
@@ -1,5 +1,30 @@
-﻿namespace RichillCapital.Api.AcceptanceTests.SignalSources;
+﻿using System.Net.Http.Json;
 
-internal class CreateSignalSourceTests
+using FluentAssertions;
+
+using RichillCapital.Contracts.SignalSources;
+
+namespace RichillCapital.Api.AcceptanceTests.SignalSources;
+
+public sealed class CreateSignalSourceTests(
+    AcceptanceTestWebApplicationFactory factory) :
+    AcceptanceTest(factory)
 {
+    [Fact]
+    public async Task When_GivenValidRequest_Should_CreateSignalSource()
+    {
+        var request = new CreateSignalSourceRequest
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Test Signal Source",
+            Description = "Test Signal Source Description",
+        };
+
+        var response = await Client.PostAsJsonAsync("api/v1/signal-sources", request);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<CreateSignalSourceResponse>();
+        result.Should().NotBeNull();
+        result!.Id.Should().Be(request.Id);
+    }
 }
