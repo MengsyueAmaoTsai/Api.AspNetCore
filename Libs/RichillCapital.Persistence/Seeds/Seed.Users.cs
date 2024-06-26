23647b2298e229194a3236a0b5b1f566e316d236
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-26 21:20:58 +0800

    add seed users

--- Libs/RichillCapital.Persistence/Seeds/Seed.Users.cs
+++ Libs/RichillCapital.Persistence/Seeds/Seed.Users.cs
@@ -1,4 +1,5 @@
 using RichillCapital.Domain.Users;
+using RichillCapital.SharedKernel.Monads;
 
 namespace RichillCapital.Persistence.Seeds;
 
@@ -10,8 +11,15 @@ internal static partial class Seed
     {
         yield return CreateUser("UID0000001", "Mengsyue Tsai", "mengsyue.tsai@outlook.com", emailConfirmed: true, lockoutEnabled: false);
         yield return CreateUser("UID0000002", "Mengsyue Tsai", "mengsyue.tsai@gmail.com", emailConfirmed: true, lockoutEnabled: false);
-        yield return CreateUser("UID0000003", "Community User", "community-user@gmail.com", emailConfirmed: true, lockoutEnabled: false);
-        yield return CreateUser("UID0000004", "Copy Trader User", "copy-trader-user@gmail.com", emailConfirmed: false, lockoutEnabled: true);
+        yield return CreateUser("UID0000003", "Trader Studio Web", "trader-studio-web@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000004", "Trader Studio Desktop", "trader-studio-desktop@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000005", "Trader Studio Mobile", "trader-studio-mobile@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000006", "Research Web", "research-web@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000007", "Trading Bot Manager Desktop", "trading-bot-manager-desktop@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000008", "Admin Web", "admin-web@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000009", "Exchange Web", "exchange-web@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000010", "Community Web", "community-web@gmail.com", emailConfirmed: true, lockoutEnabled: false);
+        yield return CreateUser("UID0000011", "Copy Trader Desktop", "copy-trader-desktop@gmail.com", emailConfirmed: false, lockoutEnabled: true);
     }
 
     internal static User CreateUser(
@@ -20,9 +28,9 @@ internal static partial class Seed
         string email,
         bool emailConfirmed,
         bool lockoutEnabled) => User.Create(
-            id: UserId.From(userId).Value,
-            name: UserName.From(name).Value,
-            email: Email.From(email).Value,
+            id: UserId.From(userId).ThrowIfFailure().Value,
+            name: UserName.From(name).ThrowIfFailure().Value,
+            email: Email.From(email).ThrowIfFailure().Value,
             emailConfirmed: emailConfirmed,
             passwordHash: DefaultPassword,
             lockoutEnabled: lockoutEnabled,
