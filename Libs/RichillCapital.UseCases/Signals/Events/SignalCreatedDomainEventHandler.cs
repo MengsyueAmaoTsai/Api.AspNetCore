78a2a8e7bf1c5a613035af7a8bf2993f935625b1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-10 00:23:59 +0800

    Get signal details and log

--- Libs/RichillCapital.UseCases/Signals/Events/SignalCreatedDomainEventHandler.cs
+++ Libs/RichillCapital.UseCases/Signals/Events/SignalCreatedDomainEventHandler.cs
@@ -2,22 +2,32 @@
 using Microsoft.Extensions.Logging;
 
 using RichillCapital.Domain;
+using RichillCapital.Domain.Common.Repositories;
+using RichillCapital.SharedKernel.Monads;
 using RichillCapital.UseCases.Common;
 
 namespace RichillCapital.UseCases.Signals.Events;
 
 internal sealed class SignalCreatedDomainEventHandler(
-    ILogger<SignalCreatedDomainEventHandler> _logger) :
+    ILogger<SignalCreatedDomainEventHandler> _logger,
+    IReadOnlyRepository<Signal> _signalRepository) :
     IDomainEventHandler<SignalCreatedDomainEvent>
 {
-    public Task Handle(
+    public async Task Handle(
         SignalCreatedDomainEvent domainEvent,
         CancellationToken cancellationToken)
     {
-        _logger.LogInformation(
-            "Signal with id {SignalId} created.",
-            domainEvent.SignalId);
+        var maybeSignal = await _signalRepository.GetByIdAsync(
+            domainEvent.SignalId,
+            cancellationToken)
+            .ThrowIfNull();
 
-        return Task.CompletedTask;
+        var signal = maybeSignal.Value;
+
+        _logger.LogInformation(
+            "Signal with id {SignalId} created at {Time} for {Symbol}.",
+            signal.Id,
+            signal.Time,
+            signal.Symbol);
     }
 }
