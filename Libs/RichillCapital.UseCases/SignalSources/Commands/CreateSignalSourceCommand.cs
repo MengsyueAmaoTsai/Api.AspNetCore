febad6786add8737e48ef22196a5213ae8a5652e
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-09 23:21:28 +0800

    Create signal source command

---
+++ Libs/RichillCapital.UseCases/SignalSources/Commands/CreateSignalSourceCommand.cs
@@ -0,0 +1,13 @@
+using RichillCapital.Domain;
+using RichillCapital.SharedKernel.Monads;
+using RichillCapital.UseCases.Abstractions;
+
+namespace RichillCapital.UseCases.SignalSources.Commands;
+
+public sealed record CreateSignalSourceCommand :
+    ICommand<ErrorOr<SignalSourceId>>
+{
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+}
---
+++ Libs/RichillCapital.UseCases/SignalSources/Commands/CreateSignalSourceCommandHandler.cs
@@ -0,0 +1,46 @@
+using RichillCapital.Domain;
+using RichillCapital.Domain.Abstractions;
+using RichillCapital.SharedKernel.Monads;
+using RichillCapital.UseCases.Abstractions;
+
+namespace RichillCapital.UseCases.SignalSources.Commands;
+
+internal sealed class CreateSignalSourceCommandHandler(
+    IRepository<SignalSource> _signalSourceRepository,
+    IUnitOfWork _unitOfWork) :
+    ICommandHandler<CreateSignalSourceCommand, ErrorOr<SignalSourceId>>
+{
+    public async Task<ErrorOr<SignalSourceId>> Handle(
+        CreateSignalSourceCommand command,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = SignalSourceId.From(command.Id);
+
+        if (validationResult.IsFailure)
+        {
+            return ErrorOr<SignalSourceId>.WithError(validationResult.Error);
+        }
+
+        var sourceId = validationResult.Value;
+
+        var now = DateTimeOffset.UtcNow;
+
+        var errorOrSignalSource = SignalSource.Create(
+            sourceId,
+            command.Name,
+            command.Description,
+            now);
+
+        if (errorOrSignalSource.HasError)
+        {
+            return ErrorOr<SignalSourceId>.WithError(errorOrSignalSource.Errors);
+        }
+
+        var signalSource = errorOrSignalSource.Value;
+
+        _signalSourceRepository.Add(signalSource);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return ErrorOr<SignalSourceId>.With(signalSource.Id);
+    }
+}
