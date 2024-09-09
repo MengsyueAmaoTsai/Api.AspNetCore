ff8c51fa3fb6372983ff9ab29e5cbbfd7bcdfc1e
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-10 01:08:30 +0800

    Update

--- Libs/RichillCapital.Contracts/Signals/CreateSignalRequest.cs
+++ Libs/RichillCapital.Contracts/Signals/CreateSignalRequest.cs
@@ -3,6 +3,6 @@ namespace RichillCapital.Contracts.Signals;
 public sealed record CreateSignalRequest
 {
     public required DateTimeOffset Time { get; init; }
-    public required string Origin { get; init; }
     public required string SourceId { get; init; }
+    public required string Origin { get; init; }
 }
