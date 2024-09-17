4446c03046be33d6a46f4442690460bdd2131bb1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-17 23:50:21 +0800

    Add order rejected status

--- Libs/RichillCapital.Domain/OrderStatus.cs
+++ Libs/RichillCapital.Domain/OrderStatus.cs
@@ -4,7 +4,8 @@ namespace RichillCapital.Domain;
 
 public sealed class OrderStatus : Enumeration<OrderStatus>
 {
-    public static readonly OrderStatus New = new(nameof(New), 1);
+    public static readonly OrderStatus New = new(nameof(New), 0);
+    public static readonly OrderStatus Rejected = new(nameof(Rejected), 1);
     public static readonly OrderStatus Executed = new(nameof(Executed), 2);
     public static readonly OrderStatus PartiallyFilled = new(nameof(PartiallyFilled), 3);
     public static readonly OrderStatus Cancelled = new(nameof(Cancelled), 4);
