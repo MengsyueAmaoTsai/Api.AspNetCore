dc112a103a93f5b494295aed9395ff57860f82a1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-18 05:02:45 +0800

    Create position

--- Libs/RichillCapital.UseCases/Executions/Events/ExecutionCreatedDomainEventHandler.cs
+++ Libs/RichillCapital.UseCases/Executions/Events/ExecutionCreatedDomainEventHandler.cs
@@ -1,20 +1,60 @@
 using Microsoft.Extensions.Logging;
 
+using RichillCapital.Domain;
+using RichillCapital.Domain.Abstractions;
 using RichillCapital.Domain.Events;
+using RichillCapital.SharedKernel.Monads;
 using RichillCapital.UseCases.Abstractions;
 
 namespace RichillCapital.UseCases.Executions.Events;
 
 internal sealed class ExecutionCreatedDomainEventHandler(
-    ILogger<ExecutionCreatedDomainEventHandler> _logger) :
+    ILogger<ExecutionCreatedDomainEventHandler> _logger,
+    IReadOnlyRepository<Execution> _executionRepository,
+    IRepository<Position> _positionRepository,
+    IUnitOfWork _unitOfWork) :
     IDomainEventHandler<ExecutionCreatedDomainEvent>
 {
-    public Task Handle(
+    public async Task Handle(
         ExecutionCreatedDomainEvent domainEvent,
         CancellationToken cancellationToken)
     {
         _logger.LogInformation("EXECUTION CREATED: {id}", domainEvent.ExecutionId);
 
-        return Task.CompletedTask;
+        var maybeExecution = await _executionRepository
+            .FirstOrDefaultAsync(
+                e => e.Id == domainEvent.ExecutionId,
+                cancellationToken)
+            .ThrowIfNull();
+
+        var execution = maybeExecution.Value;
+
+        var maybePosition = await _positionRepository
+            .FirstOrDefaultAsync(
+                p => p.Symbol == execution.Symbol,
+                cancellationToken);
+
+        if (maybePosition.IsNull)
+        {
+            var newPosition = Position
+                .Create(
+                    PositionId.NewPositionId(),
+                    execution.AccountId,
+                    execution.Symbol,
+                    execution.TradeType == TradeType.Buy ? Side.Long : Side.Short,
+                    execution.Quantity,
+                    execution.Price,
+                    execution.CreatedTimeUtc)
+                .ThrowIfError()
+                .Value;
+
+            _positionRepository.Add(newPosition);
+        }
+
+        var position = maybePosition.Value;
+
+        _positionRepository.Update(position);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ Libs/RichillCapital.UseCases/Positions/Events/PositionCreatedDomainEventHandler.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Logging;
+
+using RichillCapital.Domain.Events;
+using RichillCapital.UseCases.Abstractions;
+
+namespace RichillCapital.UseCases.Positions.Events;
+
+internal sealed class PositionCreatedDomainEventHandler(
+    ILogger<PositionCreatedDomainEventHandler> _logger) :
+    IDomainEventHandler<PositionCreatedDomainEvent>
+{
+    public Task Handle(
+        PositionCreatedDomainEvent domainEvent,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("POSITION CREATED: {id}", domainEvent.PositionId);
+
+        return Task.CompletedTask;
+    }
+}
