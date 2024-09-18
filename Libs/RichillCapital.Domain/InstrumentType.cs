939a2c043de3bf346780420ac850124ee17cedfc
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-18 14:31:17 +0800

    Add instruments

--- Libs/RichillCapital.Domain/InstrumentType.cs
+++ Libs/RichillCapital.Domain/InstrumentType.cs
@@ -5,6 +5,12 @@ namespace RichillCapital.Domain;
 public sealed class InstrumentType : Enumeration<InstrumentType>
 {
     public static readonly InstrumentType Equity = new(nameof(Equity), 1);
+    public static readonly InstrumentType Future = new(nameof(Future), 2);
+    public static readonly InstrumentType Option = new(nameof(Option), 3);
+    public static readonly InstrumentType Index = new(nameof(Index), 4);
+    public static readonly InstrumentType ForeX = new(nameof(ForeX), 5);
+    public static readonly InstrumentType CryptoCurrency = new(nameof(CryptoCurrency), 6);
+    public static readonly InstrumentType Swap = new(nameof(Swap), 7);
 
     private InstrumentType(string name, int value)
         : base(name, value)
--- Libs/RichillCapital.Infrastructure/Persistence/Configurations/InstrumentConfiguration.cs
+++ Libs/RichillCapital.Infrastructure/Persistence/Configurations/InstrumentConfiguration.cs
@@ -39,16 +39,141 @@ internal sealed class InstrumentConfiguration : IEntityTypeConfiguration<Instrum
             .IsRequired();
 
         builder.HasData([
-            CreateInstrument(
-                symbol: "NASDAQ:MSFT",
-                description: "Microsoft Corporation",
-                type: InstrumentType.Equity),
-
-            CreateInstrument(
-                symbol: "NASDAQ:AAPL",
-                description: "Apple Inc.",
-                type: InstrumentType.Equity),
+            .. CreateTaiwanStocks(),
+            .. CreateTaiwanFutures(),
+            .. CreateUSStocks(),
+            .. CreateUSFutures(),
+            .. CreateCryptoCurrencies(),
+            .. CreateCryptoPerpetuals(),
         ]);
+    }
+
+    private static IEnumerable<Instrument> CreateUSStocks()
+    {
+        yield return CreateInstrument(
+            symbol: "NASDAQ:MSFT",
+            description: "Microsoft Corporation",
+            type: InstrumentType.Equity);
+
+        yield return CreateInstrument(
+            symbol: "NASDAQ:AAPL",
+            description: "Apple Inc.",
+            type: InstrumentType.Equity);
+
+        yield return CreateInstrument(
+            symbol: "NASDAQ:AMZN",
+            description: "Amazon.com Inc.",
+            type: InstrumentType.Equity);
+
+        yield return CreateInstrument(
+            symbol: "NASDAQ:FB",
+            description: "Meta Platforms Inc.",
+            type: InstrumentType.Equity);
+    }
+
+    private static IEnumerable<Instrument> CreateUSFutures()
+    {
+        yield return CreateInstrument(
+            symbol: "CME:ES",
+            description: "E-mini S&P 500 Futures",
+            type: InstrumentType.Future);
+
+        yield return CreateInstrument(
+            symbol: "CME:NQ",
+            description: "E-mini Nasdaq 100 Futures",
+            type: InstrumentType.Future);
+
+        yield return CreateInstrument(
+            symbol: "CME:YM",
+            description: "E-mini Dow Jones Futures",
+            type: InstrumentType.Future);
+
+        yield return CreateInstrument(
+            symbol: "CME:RTY",
+            description: "E-mini Russell 2000 Futures",
+            type: InstrumentType.Future);
+    }
+
+    private static IEnumerable<Instrument> CreateTaiwanStocks()
+    {
+        yield return CreateInstrument(
+            symbol: "TWSE:2330",
+            description: "Taiwan Semiconductor Manufacturing Company Limited",
+            type: InstrumentType.Equity);
+
+        yield return CreateInstrument(
+            symbol: "TWSE:2317",
+            description: "Hon Hai Precision Industry Co., Ltd.",
+            type: InstrumentType.Equity);
+
+        yield return CreateInstrument(
+            symbol: "TWSE:2454",
+            description: "Mediatek Inc.",
+            type: InstrumentType.Equity);
+
+        yield return CreateInstrument(
+            symbol: "TWSE:2303",
+            description: "United Microelectronics Corporation",
+            type: InstrumentType.Equity);
+    }
+
+    private static IEnumerable<Instrument> CreateTaiwanFutures()
+    {
+        yield return CreateInstrument(
+            symbol: "TAIFEX:TXF",
+            description: "TSE Taiwan 50 Index Futures",
+            type: InstrumentType.Future);
+
+        yield return CreateInstrument(
+            symbol: "TAIFEX:MXF",
+            description: "Mini-TAIEX Futures",
+            type: InstrumentType.Future);
+
+        yield return CreateInstrument(
+            symbol: "TAIFEX:TEF",
+            description: "TSE Electronic Sector Index Futures",
+            type: InstrumentType.Future);
+
+        yield return CreateInstrument(
+            symbol: "TAIFEX:TFF",
+            description: "TSE Finance Sector Index Futures",
+            type: InstrumentType.Future);
+    }
+
+    private static IEnumerable<Instrument> CreateCryptoCurrencies()
+    {
+        yield return CreateInstrument(
+            symbol: "BINANCE:BTCUSDT",
+            description: "Bitcoin / Tether USD",
+            type: InstrumentType.CryptoCurrency);
+
+        yield return CreateInstrument(
+            symbol: "BINANCE:ETHUSDT",
+            description: "Ethereum / Tether USD",
+            type: InstrumentType.CryptoCurrency);
+
+        yield return CreateInstrument(
+            symbol: "BINANCE:BNBUSDT",
+            description: "Binance Coin / Tether USD",
+            type: InstrumentType.CryptoCurrency);
+    }
+
+    private static IEnumerable<Instrument> CreateCryptoPerpetuals()
+    {
+        yield return CreateInstrument(
+            symbol: "BINANCE:BTCUSD.P",
+            description: "Bitcoin / USD Perpetual",
+            type: InstrumentType.Swap);
+
+        yield return CreateInstrument(
+            symbol: "BINANCE:ETHUSD.P",
+            description: "Ethereum / USD Perpetual",
+            type: InstrumentType.Swap);
+
+        yield return CreateInstrument(
+            symbol: "BINANCE:BNBUSD.P",
+            description: "Binance Coin / USD Perpetual",
+            type: InstrumentType.Swap);
     }
 
     private static Instrument CreateInstrument(
