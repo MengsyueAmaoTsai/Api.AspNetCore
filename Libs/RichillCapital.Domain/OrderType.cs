47b892e836814439eeea161fc90e586b7643aa05
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-18 22:12:40 +0800

    add OrderType

--- Libs/RichillCapital.Domain/OrderType.cs
+++ Libs/RichillCapital.Domain/OrderType.cs
@@ -4,7 +4,11 @@ namespace RichillCapital.Domain;
 
 public sealed class OrderType : Enumeration<OrderType>
 {
-    public static readonly OrderType Market = new(nameof(Market), 1);
+    public static readonly OrderType Market = new(nameof(Market), 0);
+    public static readonly OrderType Limit = new(nameof(Limit), 1);
+    public static readonly OrderType Stop = new(nameof(Stop), 2);
+    public static readonly OrderType StopLimit = new(nameof(StopLimit), 3);
+    public static readonly OrderType TrailingStop = new(nameof(TrailingStop), 4);
 
     private OrderType(string name, int value)
         : base(name, value)
