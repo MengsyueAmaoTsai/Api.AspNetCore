3f1b7af606dcb0ef481b70945c136ca8364a12a4
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-18 22:36:21 +0800

    Save execution

--- Libs/RichillCapital.UseCases/Orders/Events/OrderExecutedDomainEventHandler.cs
+++ Libs/RichillCapital.UseCases/Orders/Events/OrderExecutedDomainEventHandler.cs
@@ -3,12 +3,14 @@ using Microsoft.Extensions.Logging;
 using RichillCapital.Domain;
 using RichillCapital.Domain.Abstractions;
 using RichillCapital.Domain.Events;
+using RichillCapital.SharedKernel.Monads;
 using RichillCapital.UseCases.Abstractions;
 
 namespace RichillCapital.UseCases.Orders.Events;
 
 internal sealed class OrderExecutedDomainEventHandler(
     ILogger<OrderExecutedDomainEventHandler> _logger,
+    IRepository<Execution> _executionRepository,
     IUnitOfWork _unitOfWork) :
     IDomainEventHandler<OrderExecutedDomainEvent>
 {
@@ -16,15 +18,42 @@ internal sealed class OrderExecutedDomainEventHandler(
         OrderExecutedDomainEvent domainEvent,
         CancellationToken cancellationToken)
     {
+        LogEvent(domainEvent);
+
+        var commission = decimal.Zero;
+        var tax = decimal.Zero;
+
+        // Persist execution record
+        var execution = Execution
+            .Create(
+                ExecutionId.NewExecutionId(),
+                domainEvent.AccountId,
+                domainEvent.OrderId,
+                domainEvent.Symbol,
+                domainEvent.TradeType,
+                domainEvent.OrderType,
+                domainEvent.TimeInForce,
+                domainEvent.Quantity,
+                domainEvent.Price,
+                commission,
+                tax,
+                domainEvent.OccurredTime)
+            .ThrowIfError()
+            .Value;
+
+        _executionRepository.Add(execution);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+
+    private void LogEvent(OrderExecutedDomainEvent domainEvent) =>
         _logger.LogInformation(
-            "ORDER EXECUTED: {tradeType} {executionQuantity} {symbol} @ {executionPrice} {orderType} {timeInForce}",
+            "[OrderExecuted] {tradeType} {quantity} {symbol} @ {price} {orderType} {timeInForce} for order id: {orderId}",
             domainEvent.TradeType,
             domainEvent.Quantity,
             domainEvent.Symbol,
             domainEvent.Price,
             domainEvent.OrderType,
-            domainEvent.TimeInForce);
-
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
-    }
+            domainEvent.TimeInForce,
+            domainEvent.OrderId);
 }
