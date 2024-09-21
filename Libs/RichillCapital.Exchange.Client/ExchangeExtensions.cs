38ffe4f19fcea3c26f9cc8050ebe9292abb5b333
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-22 07:02:54 +0800

    Add message handler

--- Libs/RichillCapital.Exchange.Client/ExchangeExtensions.cs
+++ Libs/RichillCapital.Exchange.Client/ExchangeExtensions.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
+using RichillCapital.Http;
+
 namespace RichillCapital.Exchange.Client;
 
 public static class ExchangeExtensions
 {
     public static IServiceCollection AddExchangeRestClient(this IServiceCollection services)
     {
+        services.AddDefaultRequestDebuggingMessageHandler();
+
         services
             .AddHttpClient<IExchangeRestClient, ExchangeRestClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:10000");
                 client.DefaultRequestHeaders.Clear();
-            });
+            })
+            .AddDefaultRequestDebuggingMessageHandler();
 
         return services;
     }
