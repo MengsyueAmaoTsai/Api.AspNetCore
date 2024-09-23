76e100609f4ecb264fa41b2dbc681627aaedb8a0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-23 09:08:11 +0800

    Add symbol validation logics

--- Libs/RichillCapital.Domain/Symbol.cs
+++ Libs/RichillCapital.Domain/Symbol.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 using RichillCapital.SharedKernel;
 using RichillCapital.SharedKernel.Monads;
 
@@ -5,6 +7,7 @@ namespace RichillCapital.Domain;
 
 public sealed class Symbol : SingleValueObject<string>
 {
+    private static readonly char[] AllowedCharactersInSymbolPart = ['!', '.'];
     internal const int MaxLength = 36;
 
     private Symbol(string value)
@@ -15,7 +18,29 @@ public sealed class Symbol : SingleValueObject<string>
     public static Result<Symbol> From(string value) =>
         Result<string>
             .With(value)
-            .Ensure(symbol => !string.IsNullOrEmpty(symbol), Error.Invalid($"{nameof(Symbol)} cannot be empty."))
-            .Ensure(symbol => symbol.Length <= MaxLength, Error.Invalid($"{nameof(Symbol)} cannot be longer than {MaxLength} characters."))
+            .Ensure(
+                symbol => !string.IsNullOrEmpty(symbol),
+                Error.Invalid($"{nameof(Symbol)} cannot be empty."))
+            .Ensure(
+                symbol => symbol.Length <= MaxLength,
+                Error.Invalid($"{nameof(Symbol)} cannot be longer than {MaxLength} characters."))
+            .Ensure(
+                IsValidSymbolFormat,
+                Error.Invalid($"{nameof(Symbol)} must be in the format {{exchange}}:{{ticker}}." +
+                    $" Ensure that it contains exactly one colon and both parts are valid."))
             .Then(symbol => new Symbol(symbol));
+
+    private static bool IsValidSymbolFormat(string value)
+    {
+        var regex = new Regex(@"^(?<exchange>[^:]+):(?<ticker>[a-zA-Z0-9!\.]+)$");
+        var match = regex.Match(value);
+
+        return match.Success &&
+               !string.IsNullOrEmpty(match.Groups["exchange"].Value) &&
+               IsValidTicker(match.Groups["ticker"].Value);
+    }
+
+    private static bool IsValidTicker(string value) =>
+        new Regex(@"^[a-zA-Z0-9" + Regex.Escape(new string(AllowedCharactersInSymbolPart)) + @"]+$")
+            .IsMatch(value);
 }
--- Tests/RichillCapital.Domain.UnitTests/SymbolTests.cs
+++ Tests/RichillCapital.Domain.UnitTests/SymbolTests.cs
@@ -1,11 +1,39 @@
 ﻿using FluentAssertions;
 
+using RichillCapital.SharedKernel;
+
 namespace RichillCapital.Domain.UnitTests;
 
 public sealed class SymbolTests
 {
-    [Fact]
-    public void From_Should_CreateSymbol()
+    [Theory]
+    [InlineData("")]
+    [InlineData("00000000000000000000000000000000000000000000000000000000000000000000")]
+    [InlineData("BINANCEBTCUSDT")]
+    [InlineData("BINANCE:BTCUSDT/")]
+    public void From_When_ValueIsInvalid_Should_ReturnFailureResult(
+        string invalidValue)
     {
+        var symbolResult = Symbol.From(invalidValue);
+
+        symbolResult.IsFailure.Should().BeTrue();
+
+        var error = symbolResult.Error;
+
+        error.Code.Should().Be(ErrorType.Validation.ToString());
+    }
+
+    [Theory]
+    [InlineData("BINANCE:BTCUSDT")]
+    [InlineData("BINANCE:BTCUSDT.P")]
+    [InlineData("TAIFEX:TXF!1")]
+    public void From_When_ValueIsValid_Should_ReturnSuccessResult(string value)
+    {
+        var symbolResult = Symbol.From(value);
+
+        symbolResult.IsSuccess.Should().BeTrue(symbolResult.Error.Message);
+
+        var symbol = symbolResult.Value;
+        symbol.Value.Should().Be(value);
     }
 }
