a406ac9dba36be612047f54958257292f42c9c32
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-23 18:01:17 +0800

    Add more signal origin

--- Libs/RichillCapital.Domain/SignalOrigin.cs
+++ Libs/RichillCapital.Domain/SignalOrigin.cs
@@ -5,6 +5,10 @@ namespace RichillCapital.Domain;
 public sealed class SignalOrigin :
     Enumeration<SignalOrigin>
 {
+    public static readonly SignalOrigin Swagger = new(nameof(Swagger), -1);
+    public static readonly SignalOrigin VSCodeRestClient = new(nameof(VSCodeRestClient), -2);
+    public static readonly SignalOrigin Postman = new(nameof(Postman), -3);
+
     public static readonly SignalOrigin TradingView = new("RichillCapital.Quant.TradingView", 1);
 
     private SignalOrigin(string name, int value)
