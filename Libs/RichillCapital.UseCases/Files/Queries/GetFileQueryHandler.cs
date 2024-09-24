21784ba04305e69ce654413ab22037a32f4f4736
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-25 01:02:11 +0800

    Get file

--- Libs/RichillCapital.UseCases/Files/Queries/GetFileQueryHandler.cs
+++ Libs/RichillCapital.UseCases/Files/Queries/GetFileQueryHandler.cs
@@ -1,17 +1,37 @@
 using RichillCapital.Domain.Abstractions;
+using RichillCapital.Domain.Files;
+using RichillCapital.SharedKernel;
 using RichillCapital.SharedKernel.Monads;
 using RichillCapital.UseCases.Abstractions;
 
 namespace RichillCapital.UseCases.Files.Queries;
 
 internal sealed class GetFileQueryHandler(
-    IFileStorageManager _fileStorageManager) :
+    IReadOnlyRepository<FileEntry> _fileRepository) :
     IQueryHandler<GetFileQuery, ErrorOr<FileDto>>
 {
-    public Task<ErrorOr<FileDto>> Handle(
+    public async Task<ErrorOr<FileDto>> Handle(
         GetFileQuery query,
         CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var validationResult = FileEntryId.From(query.FileId);
+
+        if (validationResult.IsFailure)
+        {
+            return ErrorOr<FileDto>.WithError(validationResult.Error);
+        }
+
+        var id = validationResult.Value;
+
+        var maybeFile = await _fileRepository.GetByIdAsync(id, cancellationToken);
+
+        if (maybeFile.IsNull)
+        {
+            return ErrorOr<FileDto>.WithError(Error.NotFound($"File with id {id} not found"));
+        }
+
+        var file = maybeFile.Value;
+
+        return ErrorOr<FileDto>.With(file.ToDto());
     }
 }
